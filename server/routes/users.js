ab00cca21ad873b9de758c71e7d5f38c1b193c86
itc2022541003 <ecogon@naver.com>
2022-06-10 10:39:48 +0900

    마지막수업:로그인할 때 보내는 데이터 id와 name으로 제한 및 헤더에서 요청하는 /info요청 처리

--- server/routes/users.js
+++ server/routes/users.js
@@ -41,6 +41,7 @@ router.post("/join", async function(req, res){
 router.post("/login", async function(req, res){
   console.log(req.body);
   const user = await User.findOne({
+    attributes:["id", "name"],
     //DB에서 req로 받은 id와 password가 일치하는 사람이 있는지 검색
     where:{
       id: req.body.id,
@@ -56,7 +57,31 @@ router.post("/login", async function(req, res){
   }
   req.session.user = user;
   res.json({
-    result:"ok"
+    result:"ok",
+    uesr: user
+  })
+})
+
+router.post("/info", async (req, res) =>{
+  //로그인 되어있을 때
+  if(req.session.user){
+    res.json({
+      result:"ok",
+      user: req.session.user
+    })
+  }
+  else{
+    //로그인되지 않았을 때
+    res.json({
+      result: "fail"
+    })
+  }  
+})
+
+router.post("/logout", async (req, res)=>{
+  req.session.destroy()
+  res.json({
+    result: "ok"
   })
 })
 
